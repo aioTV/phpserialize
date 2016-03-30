b9cdb3534df9fc4701ff9f17350c6c339493f940
Raymond Tyler Wall <rtwall@aio-tv.com>
2016-03-30 14:22:21 -0300

    update aiotv version format

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     name='phpserialize',
     author='Armin Ronacher',
     author_email='armin.ronacher@active-4.com',
-    version='1.6',
+    version='1.6+aiotv.1',
     url='https://github.com/nathanwalsh/phpserialize',
     py_modules=['phpserialize'],
     description='a port of the serialize and unserialize '
